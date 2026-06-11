54bf299d86a682e07bc6e81adc0773f278b80b57
cyi072907-cpu <cyi072907@gmail.com>
2026-06-12 00:57:40 +0800

    删除 push.js

--- static/static/push.js
+++
@@ -1,9 +0,0 @@
-function sendNotification(title, body) {
-    if (Notification.permission === "granted") {
-        new Notification(title, { body });
-    }
-}
-
-if ("Notification" in window) {
-    Notification.requestPermission();
-}
