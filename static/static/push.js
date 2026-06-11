04dcb80c7d8df943f090f34db9e3bc5d7869de83
cyi072907-cpu <cyi072907@gmail.com>
2026-06-12 00:59:24 +0800

    更新 push.js

--- static/static/push.js
+++ static/static/push.js
@@ -1,1 +1,12 @@
+if ("Notification" in window) {
+    Notification.requestPermission();
+}
 
+function push(title, msg) {
+    if (Notification.permission === "granted") {
+        new Notification(title, {
+            body: msg,
+            icon: "/static/icon.png"
+        });
+    }
+}
