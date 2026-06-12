08c962760fd5d253a3b3401fc344796c60e92084
cyi072907-cpu <cyi072907@gmail.com>
2026-06-12 17:14:56 +0800

    更新 service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,7 +1,3 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker Installed");
-});
-
 self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
